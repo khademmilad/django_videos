72674e7db78bb9fe59d98db600d83d13e82688d2
Milad Khadem <khademmilad@gmail.com>
2021-05-23 22:11:32 +0200

    media dir and add navbar in base.html

--- mysite/settings.py
+++ mysite/settings.py
@@ -136,9 +136,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "media"),
 ]
 
 # Default primary key field type
