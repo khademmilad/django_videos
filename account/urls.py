eb02c620b08013be22e4a5e3995d0ba427b4d0f6
Milad Khadem <khademmilad@gmail.com>
2021-05-29 08:04:24 +0200

    registration form

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from account.views import (
     home_view,
+    register_view
 
 )
 
@@ -8,4 +9,5 @@ app_name = 'account'
 
 urlpatterns = [
     path('', home_view, name="account_home"),
+    path('register/',register_view, name="register"),
 ]
--- account/views.py
+++ account/views.py
@@ -1,7 +1,37 @@
-from django.shortcuts import render
-
+from django.contrib.auth import authenticate
+from django.http.response import HttpResponse
+from django.shortcuts import redirect, render
+from account.forms import RegistrationForm
+from django.contrib.auth import login
 
 
 def home_view(request):
     
     return render(request, "account/home.html")
+
+
+def register_view(request, *args, **kwargs):
+    user = request.user
+    if user.is_authenticated:
+        return HttpResponse("You are already authenticate as " + str(user.email))
+    dic = {}
+    if request.POST:
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data.get('email').lower()
+            raw_password = form.cleaned_data.get('password1')
+            account = authenticate(email = email, password = raw_password)
+            login(request, account)
+            go_to_page = kwargs.get('next')
+            if go_to_page:
+                return redirect(go_to_page)
+            return redirect('account:account_home')
+        else:
+            dic['form'] = form
+    
+    else:
+        form = RegistrationForm()
+        dic['form'] = form
+        
+    return render(request, 'account/register.html', dic)
