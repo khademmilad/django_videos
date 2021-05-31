964e19c18a6858d2e8bcd1f4fb7a1e433d7cc0f6
Milad Khadem <khademmilad@gmail.com>
2021-05-31 12:05:24 +0200

    detail_video

--- video/urls.py
+++ video/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from video.views import (
     new_release_video,
+    detail_video_view
 )
 
 
 app_name = 'video'
 
 urlpatterns = [
-    path('', new_release_video, name='video_home')
+    path('', new_release_video, name='video_home'),
+    path('detail/<int:my_id>', detail_video_view, name='detail_video')
 ]
--- video/views.py
+++ video/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from video.models import Video, Subscriber
 from django.http import HttpResponse
 
@@ -13,4 +13,20 @@ def new_release_video(request):
         'queryset' : queryset
     }
     return render(request, 'video/new_release_video.html', dic)
+    
+    
+
+def detail_video_view(request, my_id):
+    video = get_object_or_404(Video, pk=my_id)
+    
+    count_likes = video.count_likes()
+    
+    video.video_views = video.video_views + 1
+    video.save()
+    
+    dic = {
+        'video' : video,
+        'count_likes' : count_likes,
+    }
+    return render(request, 'video/detail_video.html', dic)
     
